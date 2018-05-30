d3415c68386cc772a381e159571de07e8f81d499
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2018-05-30 21:29:51 +0300

    fixed bzrest menu and some links

--- frontend/config/footer-links.php
+++ frontend/config/footer-links.php
@@ -8,7 +8,7 @@ return [
         AppHelper::FOOTER_LINK_FB => 'https://www.facebook.com/profile.php?id=100021657377532',
         AppHelper::FOOTER_LINK_OK => 'https://ok.ru/profile/570782661496',
         AppHelper::FOOTER_LINK_VK => 'https://vk.com/bzfit',
-        AppHelper::FOOTER_LINK_IN => 'https://www.instagram.com/bezhitsa_fitness/',
+        AppHelper::FOOTER_LINK_IN => 'https://www.instagram.com/grandhotelbezhitsa/',
         AppHelper::FOOTER_LINK_TA => 'https://www.tripadvisor.ru/Hotel_Review-g608957-d13331291-Reviews-Grand_Hotel_Bezhitsa-Bryansk_Bryansk_Oblast_Central_Russia.html',
     ],
     Domain::DOMAIN_FITNESS_ID => [
@@ -22,7 +22,7 @@ return [
         AppHelper::FOOTER_LINK_FB => 'https://www.facebook.com/profile.php?id=100021657377532',
         AppHelper::FOOTER_LINK_OK => 'https://ok.ru/profile/570782661496',
         AppHelper::FOOTER_LINK_VK => 'https://vk.com/bzfit',
-        AppHelper::FOOTER_LINK_IN => 'https://www.instagram.com/bezhitsa_fitness/',
+        AppHelper::FOOTER_LINK_IN => 'https://www.instagram.com/bezhitsarest/',
         AppHelper::FOOTER_LINK_TA => 'https://www.tripadvisor.ru/Hotel_Review-g608957-d13331291-Reviews-Grand_Hotel_Bezhitsa-Bryansk_Bryansk_Oblast_Central_Russia.html',
     ],
 ];
