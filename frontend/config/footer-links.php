20f8d4fb461e8dfd7143a1434d306657ab75830e
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2018-05-31 11:38:57 +0300

    fix footer links restaurant

--- frontend/config/footer-links.php
+++ frontend/config/footer-links.php
@@ -20,8 +20,6 @@ return [
     ],
     Domain::DOMAIN_RESTAURANT_ID => [
         AppHelper::FOOTER_LINK_FB => 'https://www.facebook.com/profile.php?id=100023553678038',
-        AppHelper::FOOTER_LINK_OK => 'https://ok.ru/profile/570782661496',
-        AppHelper::FOOTER_LINK_VK => 'https://vk.com/bzfit',
         AppHelper::FOOTER_LINK_IN => 'https://www.instagram.com/bezhitsarest/',
         AppHelper::FOOTER_LINK_TA => 'https://www.tripadvisor.ru/Hotel_Review-g608957-d13331291-Reviews-Grand_Hotel_Bezhitsa-Bryansk_Bryansk_Oblast_Central_Russia.html',
     ],
